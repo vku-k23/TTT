1b2f672da3914ac31f63162125789ce68dfc7ff5
vietviet08 <vietnq.23ceb@vku.udn.vn>
2025-03-13 14:55:46 +0700

    fix: 🐛 fix androidmainfest file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
     id("com.google.devtools.ksp")
     id("com.google.dagger.hilt.android")
     id("kotlin-parcelize")
